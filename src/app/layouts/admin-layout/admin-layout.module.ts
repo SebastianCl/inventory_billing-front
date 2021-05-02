335966148206b57932b2c8fb5c9bb97dba00976b
Sebastian Cardona Loaiza <cardonaloaizasebastian112@gmail.com>
2021-05-02 14:35:12 -0500

    modulos de empleado

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -10,6 +10,8 @@ import { InvoiceComponent } from '../../invoice/invoice.component';
 import { ProfilesComponent } from '../../profiles/profiles.component';
 import { CustomerProfileComponent } from '../../customer-profile/customer-profile.component';
 import { ListCustomerComponent } from '../../list-customer/list-customer.component';
+import { EmployeeProfileComponent } from '../../employee-profile/employee-profile.component';
+import { ListEmployeeComponent } from '../../list-employee/list-employee.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { ListUserComponent } from '../../list-user/list-user.component';
 import { ListsComponent } from '../../lists/lists.component';
@@ -18,6 +20,7 @@ import { ListArticleComponent } from '../../list-article/list-article.component'
 import { ListReserveComponent } from '../../list-reserve/list-reserve.component';
 
 import { DetailsCustomerComponent } from '../../details-customer/details-customer.component';
+import { DetailsEmployeeComponent } from '../../details-employee/details-employee.component';
 import { DetailsUserComponent } from '../../details-user/details-user.component';
 import { DetailsArticleComponent } from '../../details-article/details-article.component';
 import { DetailsReserveComponent } from '../../details-reserve/details-reserve.component';
@@ -79,11 +82,14 @@ import { ListInvoiceComponent } from 'app/list-invoice/list-invoice.component';
     InvoiceComponent,
     ProfilesComponent,
     CustomerProfileComponent,
+    EmployeeProfileComponent,
     UserProfileComponent,
     ListUserComponent,
     ListsComponent,
     ListCustomerComponent,
     DetailsCustomerComponent,
+    ListEmployeeComponent,
+    DetailsEmployeeComponent,
     DetailsUserComponent,
     ArticleProfileComponent,
     ListArticleComponent,
